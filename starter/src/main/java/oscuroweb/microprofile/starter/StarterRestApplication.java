aa919330d67a619a225144b69a9b08b6ad2774a0
Rafael Hidalgo Calero <rafael.hidalgo.calero@gmail.com>
2021-04-02 10:44:06 +0200

    Initial commit

---
+++ starter/src/main/java/oscuroweb/microprofile/starter/StarterRestApplication.java
@@ -0,0 +1,11 @@
+package oscuroweb.microprofile.starter;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class StarterRestApplication extends Application {
+}
